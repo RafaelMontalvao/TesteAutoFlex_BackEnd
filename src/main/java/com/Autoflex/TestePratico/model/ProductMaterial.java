60f72d15568bf223033c8ecb3624df88d966a13a
Rafael Brito <rushbig@hotmail.com>
2026-02-11 15:04:44 -0300

    ajustando no da coluna raw_material_id

--- src/main/java/com/Autoflex/TestePratico/model/ProductMaterial.java
+++ src/main/java/com/Autoflex/TestePratico/model/ProductMaterial.java
@@ -15,14 +15,14 @@ public class ProductMaterial {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
-
+git
     @ManyToOne
     @JoinColumn(name="product_id")
     @JsonBackReference
     private Product product;
 
     @ManyToOne
-    @JoinColumn(name="rawn_material_id")
+    @JoinColumn(name="raw_material_id")
     private RawMaterial rawMaterial;
 
     private BigDecimal requeiredQuantity;
